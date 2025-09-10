337c13fee07a068ee0787cd20799be8e9e97d517
nfatihul7 <naufal.rpl25@hyperdata.biz>
2025-09-10 16:12:34 +0700

    hahahahahhaa

--- test.php
+++ test.php
@@ -1,1 +1,15 @@
-<?= echo "aaa"; ?>
+<?php
+
+namespace App\Controllers;
+
+class Test extends COntroller {
+
+
+    public function index()
+    {
+        $response  = $this->view->render("test/index");
+        if (!empty ($response)) {
+            echo 'Saya juga gak tau';
+        }
+    }
+}
